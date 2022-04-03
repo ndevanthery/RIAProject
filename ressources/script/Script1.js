0bc7ef82fb8cd2aec9eed9fba8551b8d3b64c23d
simon <simon.beaud@students.hevs.ch>
2022-04-03 14:26:30 +0200

    responsive task barre

---
+++ ressources/script/Script1.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
